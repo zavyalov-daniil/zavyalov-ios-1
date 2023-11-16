62940d06761c912cfa7906157eed5abb665d59ae
Daniil Zavyalov <dezavv1@gmail.com>
2023-11-16 16:43:35 +0600

    Initial Commit

---
+++ lab1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  lab1
+//
+//  Created by dezavv on 11/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ lab1/lab1App.swift
@@ -0,0 +1,17 @@
+//
+//  lab1App.swift
+//  lab1
+//
+//  Created by dezavv on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct lab1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
