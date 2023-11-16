c44bb380f5a234d9de4cf870cd178ba207bae42d
Daniil Zavyalov <dezavv1@gmail.com>
2023-11-16 21:54:03 +0600

    [+] basic app

--- lab1/ContentView.swift
+++ lab1/ContentView.swift
@@ -10,12 +10,47 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                VStack {
+                    Text("Hello,")
+                        .font(Font.custom("Poppins", size: 16))
+                        .foregroundColor(Color(red: 0.53, green: 0.59, blue: 0.73))
+                    
+                    Text("Hi James")
+                        .font(
+                            Font.custom("Poppins", size: 20)
+                                .weight(.bold)
+                        )
+                        .foregroundColor(Color(red: 0.05, green: 0.11, blue: 0.2))
+                }
+                Spacer()
+                Image("Frame").clipShape(Circle())
+                
+            }
+            .padding(.leading, 24)
+            .padding(.leading, 24)
+            HStack() {
+                Image("Doc").clipShape(Circle()).frame(width: 48, height: 48)
+                Spacer()
+                VStack(alignment: .center, spacing: 16) {
+                    Text("Dr. Imran Syahir")
+                        .font(Font.custom("Poppins", size: 14).weight(.bold))
+                        .foregroundColor(.white)
+                    Text("General Doctor")
+                        .font(Font.custom("Poppins", size: 14))
+                        .foregroundColor(Color(red: 0.8, green: 0.88, blue: 1))
+                }
+                Spacer()
+                Image("arrow-right").frame(width: 24, height: 24)
+                
+            }
+            .padding(20)
+            .frame(width: 327, alignment: .top)
+            .background(Color(red: 0.28,  green: 0.58, blue: 1))
+            .cornerRadius(12)
         }
-        .padding()
+        
+        
     }
 }
 
---
+++ lab1/SearchBar.swift
@@ -0,0 +1,13 @@
+//
+//  SearchBar.swift
+//  lab1
+//
+//  Created by dezavv on 11/16/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct SearchBar: View {
+    
+}
